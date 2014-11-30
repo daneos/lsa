700ab4c1a14c7088f50fb4f239c8ad6b30a33ea2
daneos <daneos@daneos.com>
2014-11-30 19:37:49 +0100

    version.h repair

---
+++ include/version.h
@@ -0,0 +1,12 @@
+/*
+ * lsa - list only those files you have permission to
+ * Copyright (C) 2014 daneos.
+ * Released under the GNU GPL v2 license
+ */
+
+#if !defined(__VERSION_H__)
+#define __VERSION_H__
+
+#define __VERSION "1.0.0"
+
+#endif /* __VERSION_H__ */
