e5d78bfb5ce6cd20969cc3011b9c1e6215993dee
daneos <daneos@daneos.com>
2014-11-30 16:12:39 +0100

    godlike root

--- perm_filters.c
+++ perm_filters.c
@@ -5,6 +5,7 @@
  */
 
 #include "include/perm_filters.h"
+#include "include/fmode.h"
 
 //-----------------------------------------------------------------------------
 int filter_perm(const struct stat *f, config *c)
@@ -13,6 +14,10 @@ int filter_perm(const struct stat *f, config *c)
 	int rmask = (c->p.r ? 1<<2 : 0);
 	int wmask = (c->p.w ? 1<<1 : 0);
 	int xmask = (c->p.x ? 1 : 0);
+
+	// well, root can do anything but executing not -x files, he can even pass non-x directories
+	if((c->uid == 0) && (((mask(f->st_mode, xmask<<6)) && (mask(f->st_mode, xmask<<3)) && (mask(f->st_mode, xmask))) || ftype(f->st_mode) == 'd'))
+		return 1;
 
 	// check owner bits only if user is an owner
 	if(f->st_uid == c->uid)
